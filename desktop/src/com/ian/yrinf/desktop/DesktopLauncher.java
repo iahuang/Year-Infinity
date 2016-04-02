1c4c9ed218df786405b344f972fa1bbab60ac91a
iahuang <ianhuang@bag-end.fios-router.home>
2016-04-02 16:42:10 -0400

    First Commit!

---
+++ desktop/src/com/ian/yrinf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ian.yrinf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ian.yrinf.YrInfMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YrInfMain(), config);
+	}
+}
